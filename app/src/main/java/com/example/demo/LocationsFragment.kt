0e5005ecefac01b2148018300bec0f601b153202
Leci <leca.deca@mail.ru>
2023-03-28 19:34:34 +0600

    Added Intent

--- app/src/main/java/com/example/demo/LocationsFragment.kt
+++ app/src/main/java/com/example/demo/LocationsFragment.kt
@@ -1,19 +1,24 @@
 package com.example.demo
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.LifecycleOwner
 
 class LocationsFragment : Fragment() {
 
     private val dataModel:DataModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,13 +29,29 @@ class LocationsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.findViewById<Button>(R.id.buttonSendLoc).setOnClickListener{
-            dataModel.message.value = "London"
 
+                view.findViewById<Button>(R.id.buttonSendLoc).setOnClickListener{
+            dataModel.message.value = "London"
+            (activity as MainActivity).navController.navigate(R.id.action_locationsFragment_to_forecastFragment)
         }
         super.onViewCreated(view, savedInstanceState)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_menu, menu);
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.addLocations -> {
+             val intent = Intent(activity?.applicationContext,AddActivity::class.java )
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
     companion object {
         @JvmStatic
         fun newInstance() = LocationsFragment()
--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 
 import android.widget.Toast
@@ -25,19 +26,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     private val dataModel:DataModel by viewModels ()
     lateinit var navController:NavController
-
+    lateinit var  bNav:BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bNav:BottomNavigationView = findViewById (R.id.bNav)
-
-
+         bNav = findViewById (R.id.bNav)
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-
-
-
 
         navController = findNavController(R.id.fragmentContainerView)
         bNav.setupWithNavController(navController)
@@ -50,6 +46,12 @@ class MainActivity : AppCompatActivity() {
         })
 //        val tv = findViewById<TextView>(R.id.tv)
     }
+
+
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.toolbar_menu, menu)
+//        return true
+//    }
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp()
     }
